716b75d9b20dd6005cc623cf8becaff5a3e79370
Tejas Bansal <tejasbansal6065@gmail.com>
2026-02-01 16:36:10 +0530

    contact service class

--- SchoolApp/src/main/java/com/school/controller/ContactContoller.java
+++ SchoolApp/src/main/java/com/school/controller/ContactContoller.java
@@ -1,7 +1,10 @@
 package com.school.controller;
 
+import com.school.model.Contact;
+import com.school.service.ContactService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,20 +15,33 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @Controller
 public class ContactContoller {
 
-    private static final Logger log = LoggerFactory.getLogger(ContactContoller.class);
+    private static Logger log = LoggerFactory.getLogger(ContactContoller.class);
+
+    private final ContactService contactService;
+
+    @Autowired
+    public ContactContoller(ContactService contactService) {
+        this.contactService = contactService;
+    }
 
     @RequestMapping("/contact")
     public String displayContact(){
         return "contact.html";
     }
 
+//    @RequestMapping(value = "/saveMsg",method = POST)
+//    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum, @RequestParam String email, @RequestParam String subject, @RequestParam String message){
+//        log.info("Name : " + name);
+//        log.info("Mobile Number : " + mobileNum);
+//        log.info("Email Address : " + email);
+//        log.info("Subject : " + subject);
+//        log.info("Message : " + message);
+//        return new ModelAndView("redirect:/contact");
+//    }
+
     @RequestMapping(value = "/saveMsg",method = POST)
-    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum, @RequestParam String email, @RequestParam String subject, @RequestParam String message){
-        log.info("Name : " + name);
-        log.info("Mobile Number : " + mobileNum);
-        log.info("Email Address : " + email);
-        log.info("Subject : " + subject);
-        log.info("Message : " + message);
+    public ModelAndView saveMessage(Contact contact){
+        contactService.saveMessageDetails(contact);
         return new ModelAndView("redirect:/contact");
     }
 
---
+++ SchoolApp/src/main/java/com/school/service/ContactService.java
@@ -0,0 +1,18 @@
+package com.school.service;
+
+import com.school.controller.ContactContoller;
+import com.school.model.Contact;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContactService {
+    private static Logger log = LoggerFactory.getLogger(ContactService.class);
+
+    public boolean saveMessageDetails(Contact contact){
+        boolean isSaved = true;
+        log.info(contact.toString());
+        return isSaved;
+    }
+}
