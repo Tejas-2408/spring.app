cc8a8bca99f1d5fcbcbc37646ac48a2bbbbe4898
Tejas Bansal <tejasbansal6065@gmail.com>
2026-02-02 23:01:30 +0530

    data annotation added

--- SchoolApp/src/main/java/com/school/model/Holiday.java
+++ SchoolApp/src/main/java/com/school/model/Holiday.java
@@ -1,5 +1,8 @@
 package com.school.model;
 
+import lombok.Data;
+
+@Data
 public class Holiday {
     private final String day;
     private final String reason;
@@ -11,21 +14,23 @@ public class Holiday {
     }
 
 
-    public Holiday(String day, String reason, Type type) {
-        this.day = day;
-        this.reason = reason;
-        this.type = type;
-    }
+    // Data annotation of lombok has been used therefore constructor, getter and setters are not required
 
-    public String getDay() {
-        return day;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public Type getType() {
-        return type;
-    }
+//    public Holiday(String day, String reason, Type type) {
+//        this.day = day;
+//        this.reason = reason;
+//        this.type = type;
+//    }
+//
+//    public String getDay() {
+//        return day;
+//    }
+//
+//    public String getReason() {
+//        return reason;
+//    }
+//
+//    public Type getType() {
+//        return type;
+//    }
 }
